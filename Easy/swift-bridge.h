5ac0260ae8d3140a593b6ab9fb63dd8e2202140e
zhuchao <zhuchaowe@163.com>
2014-11-04 14:11:05 +0800

    push

--- Easy/swift-bridge.h
+++ Easy/swift-bridge.h
@@ -35,11 +35,12 @@
 
 #define EZFRAME(frame)     EZLog(@"%.1f,%.1f,%.1f,%.1f",frame.origin.x,frame.origin.y,frame.size.width,frame.size.height);
 
-
-#define HOST_CONFIG 1
 //以下配置为调用Action 类必须 配置
-#define HOST_URL @"admin.maichong.me" //服务端域名:端口
-#define CLIENT   @"easyIOS"                       //自定义客户端识别
-#define CODE_KEY @"code"                          //错误码key,支持路径 如 data/code
-#define RIGHT_CODE 0                              //正确校验码
-#define MSG_KEY  @"msg"                           //消息提示msg,支持路径 如 data/msg
+//#define HOST_CONFIG 1
+//#define HOST_URL @"test-leway.zjseek.com.cn:8000" //服务端域名:端口
+//#define CLIENT   @"easyIOS"                       //自定义客户端识别
+//#define BASE_URL @"/api/"                         //基础路由，如无基础路由可为空
+//#define CODE_KEY @"code"                          //错误码key,支持路径 如 data/code
+//#define RIGHT_CODE 0                              //正确校验码
+//#define MSG_KEY  @"msg"                           //消息提示msg,支持路径 如 data/msg
+//#endif
