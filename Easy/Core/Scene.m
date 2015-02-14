fc81b1027a95e8ca90bf22151002eed5077a92a6
zhuchao <zhuchao@iosx.me>
2015-02-14 10:20:35 +0800

    inset

--- Easy/Core/Scene.m
+++ Easy/Core/Scene.m
@@ -76,6 +76,8 @@
 -(void)addSubView:(UIView *)view
            extend:(EzAlignExtend)extend{
     [self.view addSubview:view];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.edgesForExtendedLayout = UIRectEdgeNone;
     [self.view sendSubviewToBack:view];
     [view alignTopEdgeWithView:view.superview predicate:
      ((extend == EXTEND_TOP||extend == EXTEND_TOP_BOTTOM)?@"64":@"0")];
