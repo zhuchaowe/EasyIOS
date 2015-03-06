7925c264eade21f83b55433aafb56ca950cf10e2
zhuchao <zhuchao@iosx.me>
2015-03-06 17:58:18 +0800

    urlmanager

--- Extend/URLManager/URLManager.h
+++ Extend/URLManager/URLManager.h
@@ -11,8 +11,14 @@
 
 @interface URLManager : NSObject
 @property(nonatomic,retain)NSMutableDictionary *config;
++ (instancetype)manager;
 + (void)loadConfigFromPlist:(NSString *)plistPath;
 + (void)pushURLString:(NSString *)urlString animated:(BOOL)animated;
 + (void)pushURLString:(NSString *)urlString animated:(BOOL)animated replace:(BOOL)replace;
 + (void)presentURLString:(NSString *)urlString animated:(BOOL)animated;
+
++ (void)pushURL:(NSURL *)url animated:(BOOL)animated;
++ (void)pushURL:(NSURL *)url animated:(BOOL)animated replace:(BOOL)replace;
++ (void)presentURL:(NSURL *)url animated:(BOOL)animated;
+
 @end
--- Extend/URLManager/URLManager.m
+++ Extend/URLManager/URLManager.m
@@ -22,13 +22,28 @@
     [URLNavigation pushViewController:viewController animated:animated];
 }
 
++ (void)pushURL:(NSURL *)url animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromURL:url fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated];
+}
+
 + (void)pushURLString:(NSString *)urlString animated:(BOOL)animated replace:(BOOL)replace{
     UIViewController *viewController = [UIViewController initFromString:urlString fromConfig:[URLManager manager].config];
     [URLNavigation pushViewController:viewController animated:YES replace:replace];
 }
 
++ (void)pushURL:(NSURL *)url animated:(BOOL)animated replace:(BOOL)replace{
+    UIViewController *viewController = [UIViewController initFromURL:url fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated replace:replace];
+}
+
 + (void)presentURLString:(NSString *)urlString animated:(BOOL)animated{
     UIViewController *viewController = [UIViewController initFromString:urlString fromConfig:[URLManager manager].config];
+    [URLNavigation presentViewController:viewController animated:animated];
+}
+
++ (void)presentURL:(NSURL *)url animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromURL:url fromConfig:[URLManager manager].config];
     [URLNavigation presentViewController:viewController animated:animated];
 }
 @end
