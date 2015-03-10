51511a7c4bdc4771decbd3173b34d8aa39252009
zhuchao <zhuchao@iosx.me>
2015-03-10 17:21:07 +0800

    url manager

--- Extend/URLManager/URLManager.h
+++ Extend/URLManager/URLManager.h
@@ -13,12 +13,31 @@
 @property(nonatomic,retain)NSMutableDictionary *config;
 + (instancetype)manager;
 + (void)loadConfigFromPlist:(NSString *)plistPath;
+
 + (void)pushURLString:(NSString *)urlString animated:(BOOL)animated;
 + (void)pushURLString:(NSString *)urlString animated:(BOOL)animated replace:(BOOL)replace;
-+ (void)presentURLString:(NSString *)urlString animated:(BOOL)animated;
+
++ (void)pushURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated;
++ (void)pushURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated replace:(BOOL)replace;
 
 + (void)pushURL:(NSURL *)url animated:(BOOL)animated;
 + (void)pushURL:(NSURL *)url animated:(BOOL)animated replace:(BOOL)replace;
+
++ (void)pushURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated;
++ (void)pushURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated replace:(BOOL)replace;
+
 + (void)presentURL:(NSURL *)url animated:(BOOL)animated;
++ (void)presentURLString:(NSString *)urlString animated:(BOOL)animated;
+
++ (void)presentURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated;
++ (void)presentURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated;
+
++ (void)presentURL:(NSURL *)url animated:(BOOL)animated withNavigationClass:(Class)clazz;
++ (void)presentURLString:(NSString *)urlString animated:(BOOL)animated withNavigationClass:(Class)clazz;
+
++ (void)presentURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated withNavigationClass:(Class)clazz;
++ (void)presentURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated withNavigationClass:(Class)clazz;
 
 @end
+
+
--- Extend/URLManager/URLManager.m
+++ Extend/URLManager/URLManager.m
@@ -46,4 +46,63 @@
     UIViewController *viewController = [UIViewController initFromURL:url fromConfig:[URLManager manager].config];
     [URLNavigation presentViewController:viewController animated:animated];
 }
+
++ (void)presentURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromString:urlString withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation presentViewController:viewController animated:animated];
+}
+
++ (void)presentURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromURL:url withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation presentViewController:viewController animated:animated];
+}
+
++ (void)pushURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromURL:url withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated];
+}
+
++ (void)pushURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated replace:(BOOL)replace{
+    UIViewController *viewController = [UIViewController initFromURL:url withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated replace:replace];
+}
+
++ (void)pushURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated{
+    UIViewController *viewController = [UIViewController initFromString:urlString withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated];
+}
+
++ (void)pushURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated replace:(BOOL)replace{
+    UIViewController *viewController = [UIViewController initFromString:urlString withQuery:query fromConfig:[URLManager manager].config];
+    [URLNavigation pushViewController:viewController animated:animated replace:replace];
+}
+
++ (void)presentURL:(NSURL *)url animated:(BOOL)animated withNavigationClass:(Class)clazz{
+    UIViewController *viewController = [UIViewController initFromURL:url fromConfig:[URLManager manager].config];
+    UINavigationController *nav =  [[clazz alloc]initWithRootViewController:viewController];
+    [URLNavigation presentViewController:nav animated:animated];
+}
+
++ (void)presentURL:(NSURL *)url query:(NSDictionary *)query animated:(BOOL)animated withNavigationClass:(Class)clazz{
+    UIViewController *viewController = [UIViewController initFromURL:url withQuery:query fromConfig:[URLManager manager].config];
+    UINavigationController *nav =  [[clazz alloc]initWithRootViewController:viewController];
+    [URLNavigation presentViewController:nav animated:animated];
+}
+
++ (void)presentURLString:(NSString *)urlString animated:(BOOL)animated withNavigationClass:(Class)clazz{
+    
+    UIViewController *viewController = [UIViewController initFromString:urlString fromConfig:[URLManager manager].config];
+    if ([clazz isSubclassOfClass:[UINavigationController class]]) {
+        UINavigationController *nav =  [[clazz alloc]initWithRootViewController:viewController];
+        [URLNavigation presentViewController:nav animated:animated];
+    }
+}
+
++ (void)presentURLString:(NSString *)urlString query:(NSDictionary *)query animated:(BOOL)animated withNavigationClass:(Class)clazz{
+    UIViewController *viewController = [UIViewController initFromString:urlString withQuery:query fromConfig:[URLManager manager].config];
+    if ([clazz isSubclassOfClass:[UINavigationController class]]) {
+        UINavigationController *nav =  [[clazz alloc]initWithRootViewController:viewController];
+        [URLNavigation presentViewController:nav animated:animated];
+    }
+}
 @end
