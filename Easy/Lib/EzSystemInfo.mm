3f4883b204a9aa5e488df953a34a5a3a8ae27bcf
zhuchao <zhuchao@iosx.me>
2016-09-14 17:21:03 +0800

    update

--- Easy/Lib/EzSystemInfo.mm
+++ Easy/Lib/EzSystemInfo.mm
@@ -11,13 +11,17 @@
 #pragma mark -
 
 #if (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
-const BOOL IOS8_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"8.0"] != NSOrderedAscending );
-const BOOL IOS7_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"7.0"] != NSOrderedAscending );
-const BOOL IOS6_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"6.0"] != NSOrderedAscending );
-const BOOL IOS5_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"5.0"] != NSOrderedAscending );
-const BOOL IOS4_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"4.0"] != NSOrderedAscending );
-const BOOL IOS3_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"3.0"] != NSOrderedAscending );
+const BOOL IOS10_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"10.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS9_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"9.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS8_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS7_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"7.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS6_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"6.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS5_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"5.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS4_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"4.0" options:NSNumericSearch] != NSOrderedAscending );
+const BOOL IOS3_OR_LATER = ( [[[UIDevice currentDevice] systemVersion] compare:@"3.0" options:NSNumericSearch] != NSOrderedAscending );
 
+const BOOL IOS9_OR_EARLIER = !IOS10_OR_LATER;
+const BOOL IOS8_OR_EARLIER = !IOS9_OR_LATER;
 const BOOL IOS7_OR_EARLIER = !IOS8_OR_LATER;
 const BOOL IOS6_OR_EARLIER = !IOS7_OR_LATER;
 const BOOL IOS5_OR_EARLIER = !IOS6_OR_LATER;
